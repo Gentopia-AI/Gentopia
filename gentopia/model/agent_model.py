051b5dd40d6667683c2d855be5bdeb03efe412b6
Satan <liuxk2019@mail.sustech.edu.cn>
2023-06-24 00:20:40 +0800

    fix bugs

--- gentopia/model/agent_model.py
+++ gentopia/model/agent_model.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 from enum import Enum
-
 from pydantic import BaseModel
 
 #from gentopia.agent.base_agent import BaseAgent
@@ -34,25 +33,6 @@ class AgentType(Enum):
         elif _type == AgentType.openai:
             from gentopia.agent.openai import OpenAIFunctionChatAgent
             return OpenAIFunctionChatAgent
-        else:
-            raise ValueError(f"Unknown agent type: {_type}")
-
-    @staticmethod
-    def get_agent_class(_type: 'AgentType'):
-        """
-        Get agent class from agent type.
-        :param _type: agent type
-        :return: agent class
-        """
-        if _type == AgentType.REACT:
-            from gentopia.agent.react import ReactAgent
-            return ReactAgent
-        elif _type == AgentType.REWOO:
-            from gentopia.agent.rewoo import RewooAgent
-            return RewooAgent
-        elif _type == AgentType.DIRECT:
-            from gentopia.agent.vanilla import VanillaAgent
-            return VanillaAgent
         else:
             raise ValueError(f"Unknown agent type: {_type}")
 
