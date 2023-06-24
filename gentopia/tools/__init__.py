13a0c240d2fb7b82eac763c0c8b957d0cf28f290
billxbf <billxbf@gmail.com>
2023-06-23 22:38:06 -0400

    init

--- gentopia/tools/__init__.py
+++ gentopia/tools/__init__.py
@@ -1,20 +1,15 @@
 from .basetool import BaseTool
 from .calculator import Calculator
-from .doc_store_look_up import DocStoreLookUp
-# from .dummy_tool import DummyTool
 from .google_search import GoogleSearch
 from .llm import LLM
 from .search_doc import SearchDoc
 from .wikipedia import Wikipedia
 from .wolfram_alpha import WolframAlpha
-from .zip_code import ZipCodeRetriever
 
 
 def load_tools(name: str) -> BaseTool:
     if name == "calculator":
         return Calculator
-    elif name == "doc_store_look_up":
-        return DocStoreLookUp
     elif name == "google_search":
         return GoogleSearch
     elif name == "llm":
@@ -25,7 +20,5 @@ def load_tools(name: str) -> BaseTool:
         return Wikipedia
     elif name == "wolfram_alpha":
         return WolframAlpha
-    elif name == "zip_code":
-        return ZipCodeRetriever
     else:
         raise NotImplementedError
