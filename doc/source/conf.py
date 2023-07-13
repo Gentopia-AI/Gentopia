ced0aa44db207164f8582459a4e00717ac4dfefa
Satan <liuxk2019@mail.sustech.edu.cn>
2023-07-13 17:47:28 +0800

    refine doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath(''))
 
 project = 'Gentopia'
 copyright = '2023, Binfeng Xu, Xukun Liu, Jerry Peng, Yuhan Li'
