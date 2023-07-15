65647642ccb0018f0d51f537b7fec70c4f6988a6
Satan <liuxk2019@mail.sustech.edu.cn>
2023-07-15 16:31:28 +0800

    refine doc

--- gentopia/output/base_output.py
+++ gentopia/output/base_output.py
@@ -10,73 +10,324 @@ from gentopia.output import check_log
 
 
 class BaseOutput:
+    """
+        Base class for output handlers.
+
+        Attributes:
+        -----------
+        logger : logging.Logger
+            The logger object to log messages.
+
+        Methods:
+        --------
+        stop():
+            Stop the output.
+
+        update_status(output: str, **kwargs):
+            Update the status of the output.
+
+        thinking(name: str):
+            Log that a process is thinking.
+
+        done(_all=False):
+            Log that the process is done.
+
+        stream_print(item: str):
+            Not implemented.
+
+        json_print(item: Dict[str, Any]):
+            Log a JSON object.
+
+        panel_print(item: Any, title: str = "Output", stream: bool = False):
+            Log a panel output.
+
+        clear():
+            Not implemented.
+
+        print(content: str, **kwargs):
+            Log arbitrary content.
+
+        format_json(json_obj: str):
+            Format a JSON object.
+
+        debug(content: str, **kwargs):
+            Log a debug message.
+
+        info(content: str, **kwargs):
+            Log an informational message.
+
+        warning(content: str, **kwargs):
+            Log a warning message.
+
+        error(content: str, **kwargs):
+            Log an error message.
+
+        critical(content: str, **kwargs):
+            Log a critical message.
+    """
 
     def __init__(self):
+        """
+        Initialize the BaseOutput object.
+
+        Parameters:
+        -----------
+        None
+
+        Returns:
+        --------
+        None
+        """
         self.logger = logging
 
     def stop(self):
+        """
+        Stop the output.
+
+        Parameters:
+        -----------
+        None
+
+        Returns:
+        --------
+        None
+        """
         pass
 
     def update_status(self, output: str, **kwargs):
+        """
+        Update the status of the output.
+
+        Parameters:
+        -----------
+        output : str
+            The output message.
+
+        Returns:
+        --------
+        None
+        """
         if check_log():
             self.logger.info(output)
 
 
     def thinking(self, name: str):
+        """
+        Log that a process is thinking.
+
+        Parameters:
+        -----------
+        name : str
+            The name of the process.
+
+        Returns:
+        --------
+        None
+        """
         if check_log():
             self.logger.info(f"{name} is thinking...")
 
 
     def done(self, _all=False):
+        """
+        Log that the process is done.
+
+        Parameters:
+        -----------
+        _all : bool, optional
+            Not used, defaults to False.
+
+        Returns:
+        --------
+        None
+        """
+
         if check_log():
             self.logger.info("Done")
 
 
     def stream_print(self, item: str):
+        """
+        Stream print.
+
+        Parameters:
+        -----------
+        item : str
+            Not used.
+
+        Returns:
+        --------
+        None
+        """
+
         pass
 
     def json_print(self, item: Dict[str, Any]):
+        """
+        Log a JSON object.
+
+        Parameters:
+        -----------
+        item : Dict[str, Any]
+            The JSON object to log.
+
+        Returns:
+        --------
+        None
+        """
         if check_log():
             self.logger.info(json.dumps(item, indent=2))
 
     def panel_print(self, item: Any, title: str = "Output", stream: bool = False):
+        """
+        Log a panel output.
+
+        Parameters:
+        -----------
+        item : Any
+            The item to log.
+        title : str, optional
+            The title of the panel, defaults to "Output".
+        stream : bool, optional
+            Not used, defaults to False.
+
+        Returns:
+        --------
+        None
+        """
         if check_log():
             self.logger.info('-'*20)
             self.logger.info(item)
             self.logger.info('-' * 20)
 
     def clear(self):
+        """
+        Not implemented.
+
+        Parameters:
+        -----------
+        None
+
+        Returns:
+        --------
+        None
+        """
         pass
 
     def print(self, content: str, **kwargs):
+        """
+        Log arbitrary content.
+
+        Parameters:
+        -----------
+        content : str
+            The content to log.
+
+        Returns:
+        --------
+        None
+        """
         if check_log():
             self.logger.info(content)
 
     def format_json(self, json_obj: str):
+        """
+        Format a JSON object.
+
+        Parameters:
+        -----------
+        json_obj : str
+            The JSON object to format.
+
+        Returns:
+        --------
+        str
+            The formatted JSON object.
+        """
         formatted_json = json.dumps(json_obj, indent=2)
         return formatted_json
 
 
     def debug(self, content: str, **kwargs):
+        """
+        Log a debug message.
+
+        Parameters:
+        -----------
+        content : str
+            The message to log.
+
+        Returns:
+        --------
+        None
+        """
         if check_log():
             self.logger.debug(content, **kwargs)
 
 
     def info(self, content: str, **kwargs):
+        """
+        Log an informational message.
+
+        Parameters:
+        -----------
+        content : str
+            The message to log.
+
+        Returns:
+        --------
+        None
+        """
         if check_log():
             self.logger.info(content, **kwargs)
 
 
     def warning(self, content: str, **kwargs):
+        """
+        Log a warning message.
+
+        Parameters:
+        -----------
+        content : str
+            The message to log.
+
+        Returns:
+        --------
+        None
+        """
         if check_log():
             self.logger.warning(content, **kwargs)
 
 
     def error(self, content: str, **kwargs):
+        """
+        Log an error message.
+
+        Parameters:
+        -----------
+        content : str
+            The message to log.
+
+        Returns:
+        --------
+        None
+        """
         if check_log():
             self.logger.error(content, **kwargs)
 
 
     def critical(self, content: str, **kwargs):
+        """
+        Log a critical message.
+
+        Parameters:
+        -----------
+        content : str
+            The message to log.
+
+        Returns:
+        --------
+        None
+        """
         if check_log():
             self.logger.critical(content, **kwargs)
--- gentopia/output/print_output.py
+++ gentopia/output/print_output.py
@@ -7,23 +7,52 @@ from pydantic import BaseModel
 from gentopia.model.completion_model import BaseCompletion
 
 
-class BaseOutput:
+class BasicOutput:
+    """
+        A class for handling output.
+    """
 
     def __init__(self):
+        """
+        Initializes a new instance of the BaseOutput class.
+        """
         self.status_stack: List[str] = []
 
     def stop(self):
+        """
+        Stops the output.
+        """
         pass
 
     def update_status(self, output: str, **kwargs):
+        """
+        Updates the status.
+
+        :param output: The output to update the status with.
+        :type output: str
+        :param kwargs: Additional keyword arguments to pass to the update_status method.
+        :type kwargs: Any
+        """
         self.status_stack.append(output)
         print(self.status_stack, flush=True)
 
     def thinking(self, name: str):
+        """
+        Shows that a name is thinking.
+
+        :param name: The name of the entity that is thinking.
+        :type name: str
+        """
         self.status_stack.append(name)
         print(f"{name} is thinking...", flush=True)
 
     def done(self, _all=False):
+        """
+        Marks the output as done.
+
+        :param _all: If True, marks all output as done. If False, marks only the last output as done. Defaults to False.
+        :type _all: bool
+        """
         if _all:
             self.status_stack = []
 
@@ -34,21 +63,62 @@ class BaseOutput:
                 print(f"{self.status_stack[-1]} is thinking...")
 
     def stream_print(self, item: str):
+        """
+        Prints an item to the output as a stream.
+
+        :param item: The item to print.
+        :type item: str
+        """
         print(item, end="", flush=True)
 
     def json_print(self, item: Dict[str, Any]):
+        """
+        Prints an item to the output as a JSON object.
+
+        :param item: The item to print.
+        :type item: Dict[str, Any]
+        """
         print(item, end="", flush=True)
 
     def panel_print(self, item: Any, title: str = "Output", stream: bool = False):
+        """
+        Prints an item to the output as a panel.
+
+        :param item: The item to print.
+        :type item: Any
+        :param title: The title of the panel. Defaults to "Output".
+        :type title: str
+        :param stream: If True, prints the item as a stream. If False, prints the item as a panel. Defaults to False.
+        :type stream: bool
+        """
         print(title.center(80, "-"), flush=True)
         print(item, flush=True)
 
     def clear(self):
+        """
+        Clears the output.
+        """
         pass
 
     def print(self, content: str, **kwargs):
+        """
+        Prints content to the output.
+
+        :param content: The content to print.
+        :type content: str
+        :param kwargs: Additional keyword arguments to pass to the print method.
+        :type kwargs: Any
+        """
         print(content, **kwargs)
 
     def format_json(self, json_obj: str):
+        """
+        Formats a JSON object.
+
+        :param json_obj: The JSON object to format.
+        :type json_obj: str
+        :return: The formatted JSON object.
+        :rtype: str
+        """
         formatted_json = json.dumps(json_obj, indent=2)
         return formatted_json
